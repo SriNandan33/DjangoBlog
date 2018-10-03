142966a394805b16fd27595ab525b6c15692cc1f
= <=>
2018-10-03 16:02:12 +0530

    post drafts and delete functionalities

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,14 @@ from .models import Post
 from .forms import PostForm
 
 def post_list(request):
-	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 	return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_draft_list(request):
+	posts = Post.objects.filter(published_date__isnull=True).order_by('-created_date')
+	return render(request, 'blog/post_list.html', {'posts': posts})
+
 
 def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
@@ -18,7 +24,6 @@ def post_new(request):
 		if form.is_valid():
 			post = form.save(commit=False)
 			post.author = request.user
-			post.published_date = timezone.now()
 			post.save()
 			return redirect('post_detail', pk=post.pk)
 	else:
@@ -33,10 +38,20 @@ def post_edit(request, pk):
 		if form.is_valid():
 			post = form.save(commit=False)
 			post.author = request.user
-			post.published_date = timezone.now()
 			post.save()
 			return redirect('post_detail', pk=post.pk)
 	else:
 		form = PostForm(instance=post)
 
 	return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_publish(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	post.publish()
+	return redirect('post_detail', pk=post.pk)
+
+def post_remove(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	post.delete()
+	return redirect('post_list')
