e64261675bf48c7e09da7d1a9030f90c08660263
= <=>
2018-10-03 21:59:47 +0530

    fix error

--- mysite/settings.py
+++ mysite/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_range=500)
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 
