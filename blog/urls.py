800eb4b88f185dfb1d09d9d73bbd14ff361428ab
= <=>
2018-10-03 17:40:08 +0530

    add comments to posts

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
 	path('post/<int:pk>', views.post_detail, name='post_detail'),
 	path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
 	path('post/<int:pk>/remove/', views.post_remove, name='post_remove'),
+	path('post/<int:pk>/comment', views.add_comment_to_post, name='add_comment_to_post'),
 	path('post/new', views.post_new, name='post_new'),
 	path('post/edit/<int:pk>', views.post_edit, name='post_edit'),
+	path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+	path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
 ]
